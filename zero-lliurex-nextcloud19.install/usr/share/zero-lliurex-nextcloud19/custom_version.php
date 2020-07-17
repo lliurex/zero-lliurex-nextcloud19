a0cde45865f3da83c21e0ecac14d467ea576a424
jrpelegrina <juapesai@hotmail.com>
2020-07-17 12:56:20 +0200

    Updated to nextloud 19.0.1

--- zero-lliurex-nextcloud19.install/usr/share/zero-lliurex-nextcloud19/custom_version.php
+++ zero-lliurex-nextcloud19.install/usr/share/zero-lliurex-nextcloud19/custom_version.php
@@ -1,12 +1,12 @@
 <?php 
-$OC_Version = array(19,0,0,12);
-$OC_VersionString = '19.0.0';
+$OC_Version = array(19,0,1,1);
+$OC_VersionString = '19.0.1';
 $OC_Edition = '';
 $OC_Channel = 'stable';
 $OC_VersionCanBeUpgradedFrom = array (
   'nextcloud' => 
   array (
-  	'15.0'=>true,
+    '15.0'=>true,
     '18.0' => true,
     '19.0' => true,
   ),
@@ -14,5 +14,6 @@ $OC_VersionCanBeUpgradedFrom = array (
   array (
   ),
 );
-$OC_Build = '2020-05-26T08:11:09+00:00 9c63433cde916cd35b0708b398910cbc1cb52315';
+$OC_Build = '2020-07-15T20:25:59+00:00 d7998fa123ae97be6abb31328ca1382a8a5ec31b';
 $vendor = 'nextcloud';
+
